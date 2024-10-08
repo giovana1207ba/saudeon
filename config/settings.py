e651e256f1bfc323c8be439d4576915fccbc68f3
caiomaz <caiotmzferreira@gmail.com>
2024-10-08 20:37:16 -0300

    atualizado 100% ajuda ai po

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,12 +127,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(STATIC_URL, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
